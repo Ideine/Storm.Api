6792c62f91dd19b7dff87f370397f4ea83d5875c
Valentin Jubert <valentin.jubert@ideine.fr>
2023-02-07 16:43:39 +0100

    remove null on json

--- src/Storm.Api/Launchers/BaseStartup.cs
+++ src/Storm.Api/Launchers/BaseStartup.cs
@@ -66,6 +66,7 @@ namespace Storm.Api.Launchers
 				.AddNewtonsoftJson(options =>
 				{
 					options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+					options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
 					options.SerializerSettings.ContractResolver = new DefaultContractResolver
 					{
 						NamingStrategy = new DefaultNamingStrategy()
